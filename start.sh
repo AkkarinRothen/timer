038c91e084c2552ec69850c3524455ed86da2296
AkkarinRothen <yacris245@gmail.com>
2025-07-23 11:56:12 -0300

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Start a simple HTTP server to host the timer
+PORT=${PORT:-8000}
+DIR="$(dirname "$0")"
+cd "$DIR"
+
+# Launch the built‑in Python server
+python3 -m http.server "$PORT"
+
+chmod +x start.sh
+./start.sh
